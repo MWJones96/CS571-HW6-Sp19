1e0b3a8b404c827cde9eb2091302338c688756bb
Matthew Jones <eighthmatthewjones@gmail.com>
2019-02-17 22:46:01 -0800

    More changes

--- yertdgbtrdgfbrttrh.php
+++ yertdgbtrdgfbrttrh.php
@@ -68,19 +68,62 @@
 			width: 65px;
 			height: 25px;
 		}
+
+		#results-table,
+		{
+			padding: 0;
+			margin: auto;
+			width: 1000px;
+		}
+
 	</style>
 </head>
 <body>
+	<?php
+		function getJSON()
+		{
+			$kwd = "";
+			$category = "";
+			$condition_new = "";
+			$condition_used = "";
+			$condition_unspec = "";
+
+			$local_pickup= "";
+			$free_shipping = "";
+
+			if (!empty($_POST))
+			{
+				$kwd = $_POST["keyword"];
+				$category = $_POST["category"];
+
+				$condition_new = isset($_POST["new"]) ? 1 : 0;
+				$condition_used = isset($_POST["used"]) ? 1 : 0;
+				$condition_unspec = isset($_POST["unspec"]) ? 1 : 0;
+
+				$local_pickup = isset($_POST["local"]) ? 1 : 0;
+				$free_shipping = isset($_POST["free"]) ? 1 : 0;
+
+				//The URL of the API call
+				$_API_URL = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=MatthewJ-CS571-PRD-2f2cd4cf7-09303b6c&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&paginationInput.entriesPerPage=20&keywords=usc&itemFilter.name=FreeShippingOnly&itemFilter.value=true&itemFilter.name=LocalPickupOnly&itemFilter.value=true&itemFilter.name=Condition&itemFilter.value=New&itemFilter.value=Used&itemFilter.value=Unspecified&buyerPostalCode=90007&itemFilter.name=MaxDistance&itemFilter.value=10&itemFilter.name=HideDuplicateItems&itemFilter.value=true";
+
+				//Call API
+				$json = file_get_contents($_API_URL);
+				return $json;
+			}
+
+			return NULL;
+		}
+	?>
 	<form id="main-frame" method="post" onsubmit="return false;">
 		<h1><i>Product Search</i></h1>
 		<hr>
 		<div>
 			<h3>Keyword</h3>
-			<input id="keyword" input type="textarea" name="keyword-entry" required></input>
+			<input name="keyword" id="keyword" input type="textarea" name="keyword-entry" required></input>
 		</div>
 		<div>
 			<h3>Category</h3>
-			<select id="category">
+			<select name="category" id="category">
 				<option value="all">All Categories</option>
 				<option value="550">Art</option>
 				<option value="2984">Baby</option>
@@ -94,14 +137,14 @@
 		</div>
 		<div>
 			<h3>Condition</h3>
-			<input id="new" type="checkbox" name="condition">New</input>
-			<input id="used" type="checkbox" name="condition">Used</input>
-			<input id="unspecified" type="checkbox" name="condition">Unspecified</input>
+			<input id="new" type="checkbox" name="new">New</input>
+			<input id="used" type="checkbox" name="used">Used</input>
+			<input id="unspecified" type="checkbox" name="unspec">Unspecified</input>
 		</div>
 		<div>
 			<h3>Shipping Options</h3>
-			<input id="local" type="checkbox" name="shipping">Local Pickup</input>
-			<input id="free-shipping" type="checkbox" name="shipping">Free Shipping</input>
+			<input id="local" type="checkbox" name="local">Local Pickup</input>
+			<input id="free-shipping" type="checkbox" name="free">Free Shipping</input>
 		</div>
 		<table class="nearby-search">
 			<tr>
@@ -125,16 +168,49 @@
 		</table>
 		<br>
 		<div class="buttons">
-			<input type="submit" name="search" value="Search"></input>
+			<input type="submit" name="submit" value="Search" onclick="submitForm()"></input>
 			<input type="button" name="clear" value="Clear" onclick="clearForm()"></input>
 		</div>
 	</form>
-	<div id="results-table">
-	</div>
+	<br>
+	<table id="results-table">
+	</table>
+
 	<script type="text/javascript">
+		function submitForm()
+		{
+			var json = <?php echo getJSON(); ?>;
+
+			var html_text = "";
+			html_text += "<th>";
+			html_text += "<td><strong>Index</strong></td>";
+			html_text += "<td><strong>Photo</strong></td>";
+			html_text += "<td><strong>Name</strong></td>";
+			html_text += "<td><strong>Price</strong></td>";
+			html_text += "<td><strong>Zip code</strong></td>";
+			html_text += "<td><strong>Condition</strong></td>";
+			html_text += "<td><strong>Shipping option</strong></td>";
+			html_text += "</th>";
+
+			var items = json.findItemsAdvancedResponse[0].searchResult[0].item;
+
+			for (i = 0; i < items.length; i++)
+			{
+				html_text += "<tr>";
+				//html_text += "<td>" + (i+1) + "</td>";
+				//html_text += "<td><a href=\"" + items[i].galleryURL[0] + "\"/></td>";
+				//html_text += "<td>" + items[i].title[0] +"</td>";
+				//html_text += "<td>$" + items[i].sellingStatus[0].currentPrice[0].__value__ +"</td>";
+				//html_text += "<td>" + items[i].postalCode[0] +"</td>";
+				html_text += "</tr>";
+			}
+
+			document.getElementById("results-table").innerHTML = html_text;
+		}
+
 		function disableZipReq()
 		{
-			document.getElementById("zip").required = false;
+			document.getElementById("zip").required = 0;
 		}
 
 		function enableZipReq()
@@ -151,7 +227,7 @@
 				var fields = document.getElementsByClassName("cond-fields");
 				for (i = 0; i < fields.length; i++)
 				{
-					fields[i].disabled = false;
+					fields[i].disabled = 0;
 				}
 
 			}
@@ -173,13 +249,8 @@
 				fields[i].disabled = true;
 			}
 			document.getElementById("main-frame").reset();
+			document.getElementById("results-table").innerHTML = "";
 		}
 	</script>
-	<?php
-		if (isset($_POST['main-frame']))
-		{
-			echo "Submit";
-		}
-	?>
 </body>
 </html>
