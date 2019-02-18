489e71b05e2c9b12e53211a459b49436fa271c7b
Matthew Jones <eighthmatthewjones@gmail.com>
2019-02-17 17:48:40 -0800

    Finished form (for real this time)

--- yertdgbtrdgfbrttrh.php
+++ yertdgbtrdgfbrttrh.php
@@ -11,121 +11,119 @@
 			margin: auto;
 		}
 
+		#main-frame h1
+		{
+			text-align: center;
+			margin-bottom: 15px;
+		}
+
 		#main-frame hr
 		{
 			width: 98%;
 		}
 
-		#main-frame h1
+		#main-frame div
 		{
-			text-align: center;
-			margin: 7px;
+			padding-left: 15px;
+			display: flex;
+			height: 22px;
+			align-items: center;
+			margin-bottom: 5px;
 		}
 
-		#main-frame ul
+		#main-frame div input, select
 		{
-			margin: 0;
-			padding: 0;
-		}
-
-		#main-frame ul li
-		{
-			display: inline-block;
+			height: 18px;
 			margin-left: 10px;
-			padding: 0;
-			height: 20px;
 		}
 
-		#main-frame ul ul li
+		#main-frame div ul
 		{
-			display: block;
+			list-style-type: none;
 			padding: 0;
 		}
 
-		#main-frame ul ul ul li
+		#main-frame div strong
 		{
-			display: inline-block;
-			padding: 0;
+			margin-left: 5px;
+			margin-right: 5px;
 		}
 
-		.vertical-list
+		#main-frame .nearby-search
 		{
+			margin-left: 20px;
+		}
+
+		#main-frame .buttons
+		{
+			padding: 0;
 			margin: 0;
-			padding: 0;
-			float: left;
-		}
 
-		.vertical-list li
-		{
-			margin: 0;
-		}
-
-		.buttons
-		{
-			text-align: center;
 			margin-bottom: 25px;
+			justify-content: center;
 		}
 
-		.buttons input
+		#main-frame .buttons input
 		{
-			margin: 2px;
-		}
-
-		.nearby-search li
-		{
-			vertical-align: top;
-			margin: 0px;
+			width: 65px;
+			height: 25px;
 		}
 	</style>
 </head>
 <body>
-	<?php
-	?>
-	<form id="main-frame" onsubmit="submitForm()">
-	<h1><i>Product Search</i></h1>
-	<hr>
-		<ul>
-			<li><h3>Keyword</h3></li>
-			<li><input id="keyword" input type="textarea" name="keyword-entry" required></input></li>
-		</ul>
-		<ul>
-			<li><h3>Category</h3></li>
-			<li>
-				<select id="category">
-					<option value="all">All Categories</option>
-					<option value="550">Art</option>
-					<option value="2984">Baby</option>
-					<option value="267">Books</option>
-					<option value="11450">Clothing, Shoes & Accessories</option>
-					<option value="58058">Computers/Tablets & Networking</option>
-					<option value="26395">Helath & Beauty</option>
-					<option value="11233">Music</option>
-					<option value="1249">Video Games & Consoles</option>
-				</select>
-			</li>
-		</ul>
-		<ul>
-			<li><h3>Condition</h3></li>
-			<li><input id="new" type="checkbox" name="condition">New</input></li>
-			<li><input id="used" type="checkbox" name="condition">Used</input></li>
-			<li><input id="unspecified" type="checkbox" name="condition">Unspecified</input></li>
-		</ul>
-		<ul>
-			<li><h3>Shipping Options</h3></li>
-			<li><input id="local" type="checkbox" name="shipping">Local Pickup</input></li>
-			<li><input id="free-shipping" type="checkbox" name="shipping">Free Shipping</input></li>
-		</ul>
+	<form id="main-frame" method="post" onsubmit="return false;">
+		<h1><i>Product Search</i></h1>
+		<hr>
+		<div>
+			<h3>Keyword</h3>
+			<input id="keyword" input type="textarea" name="keyword-entry" required></input>
+		</div>
+		<div>
+			<h3>Category</h3>
+			<select id="category">
+				<option value="all">All Categories</option>
+				<option value="550">Art</option>
+				<option value="2984">Baby</option>
+				<option value="267">Books</option>
+				<option value="11450">Clothing, Shoes & Accessories</option>
+				<option value="58058">Computers/Tablets & Networking</option>
+				<option value="26395">Helath & Beauty</option>
+				<option value="11233">Music</option>
+				<option value="1249">Video Games & Consoles</option>
+			</select>
+		</div>
+		<div>
+			<h3>Condition</h3>
+			<input id="new" type="checkbox" name="condition">New</input>
+			<input id="used" type="checkbox" name="condition">Used</input>
+			<input id="unspecified" type="checkbox" name="condition">Unspecified</input>
+		</div>
+		<div>
+			<h3>Shipping Options</h3>
+			<input id="local" type="checkbox" name="shipping">Local Pickup</input>
+			<input id="free-shipping" type="checkbox" name="shipping">Free Shipping</input>
+		</div>
+		<table class="nearby-search">
+			<tr>
+				<td>
+					<input id="enable-search" type="checkbox" name="nearby" onchange="enableNearbySearch()"></input>
+					<strong>Enable Nearby Search</strong>
+					<input class="cond-fields" type="number" name="miles" placeholder="10" disabled></input>
+					<strong>miles from</strong>
+				</td>
+				<td>
+					<input class="cond-fields" type="radio" name="location" onclick = "disableZipReq()" disabled checked>Here</input>
+				</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td>
+					<input class="cond-fields" type="radio" name="location" onclick = "enableZipReq()"disabled></input>
+					<input id="zip" class="cond-fields" type="number" name="zip" placeholder="zip code" disabled></input>
+				</td>
+			</tr>
+		</table>
 		<br>
-		<ul class="nearby-search">
-			<li><input id="enable-search" type="checkbox" name="nearby" onchange="enableNearbySearch()"><strong>Enable Nearby Search</strong></input></li>
-			<li><input class="cond-fields" type="number" name="miles" placeholder="10" disabled></input><li><strong>miles from</strong></li>
-			<li><ul class="vertical-list">
-				<input class="cond-fields" type="radio" name="location" onclick = "disableZipReq()" disabled checked>Here</input></li>
-				<input class="cond-fields" type="radio" name="location" onclick = "enableZipReq()"disabled></input>
-				<input id="zip" class="cond-fields" type="number" name="zip" placeholder="zip code" disabled></input>
-			</ul></li>
-		</ul>
-	<br><br>
 		<div class="buttons">
 			<input type="submit" name="search" value="Search"></input>
 			<input type="button" name="clear" value="Clear" onclick="clearForm()"></input>
@@ -142,25 +140,6 @@
 		function enableZipReq()
 		{
 			document.getElementById("zip").required = true;
-		}
-
-		function submitForm()
-		{
-			var keyword = document.getElementById("keyword").value;
-
-			var category = document.getElementById("category").value;
-
-			var newQ = document.getElementById("new").checked;
-
-			var used = document.getElementById("used").checked;
-
-			var unspecified = document.getElementById("unspecified").checked;
-
-			var local = document.getElementById("local").checked;
-
-			var freeShipping = document.getElementById("free-shipping").checked;
-
-			alert(keyword + " " + category + " " + newQ + " " + used + " " + unspecified + " " + local + " " + freeShipping);
 		}
 
 		function enableNearbySearch()
@@ -196,5 +175,11 @@
 			document.getElementById("main-frame").reset();
 		}
 	</script>
+	<?php
+		if (isset($_POST['main-frame']))
+		{
+			echo "Submit";
+		}
+	?>
 </body>
 </html>
